30a87eb0b5c83facc9f085d6beaaa5116c89564e
ArabGangDev <41093102+ArabGangDev@users.noreply.github.com>
2019-03-29 18:36:59 +0300

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,66 @@
+const Discord = require('discord.js');
+const moment = require("moment");  
+const fs = require("fs");      
+const client = new Discord.Client(); 
+const Canvas = require("canvas");
+const prefix = "." 
+
+client.on('ready', () => {
+    client.user.setGame(`in wolf shop`, 'https://twitch.tv/ًWolf-Shop');
+    console.log('Always Ready To Help!');
+});
+
+
+client.on('message', message => {//Probc
+              if(!message.channel.guild) return;
+    if(message.content.startsWith('+pbc')) {
+    if(!message.channel.guild) return message.channel.send('**هذا الأمر فقط للسيرفرات**').then(m => m.delete(5000));
+  if(!message.member.hasPermission('ADMINISTRATOR')) return      message.channel.send('**للأسف لا تمتلك صلاحية** `ADMINISTRATOR`' );
+    let args = message.content.split(" ").join(" ").slice(3 + prefix.length);
+    let copy = "Snow Bot";
+    let request = `Requested By ${message.author.username}`;
+    if (!args) return message.reply('**يجب عليك كتابة كلمة او جملة لإرسال البرودكاست**');message.channel.send(`**هل أنت متأكد من إرسالك البرودكاست؟ \nمحتوى البرودكاست:** \` ${args}\``).then(msg => {
+    msg.react('✅')
+    .then(() => msg.react('❌'))
+    .then(() =>msg.react('✅'))
+    
+    let reaction1Filter = (reaction, user) => reaction.emoji.name === '✅' && user.id === message.author.id;
+    let reaction2Filter = (reaction, user) => reaction.emoji.name === '❌' && user.id === message.author.id;
+	      let reaction1 = msg.createReactionCollector(reaction1Filter, { time: 20000 });
+    let reaction2 = msg.createReactionCollector(reaction2Filter, { time: 20000 });
+    reaction1.on("collect", r => {
+    message.channel.send(`تم ارسال البرودكاست الى ${message.guild.members.size} عضو | ✅`).then(m => m.delete(5000));
+    message.guild.members.forEach(m => {
+    var pbc = new
+       Discord.RichEmbed()
+       .setColor('RANDOM')
+       .setTitle('Broadcast')
+       .addField('Server', message.guild.name)
+       .addField('Sender', message.author.username)
+       .addField('Message', args)
+       .setThumbnail(message.author.avatarURL)
+       .setFooter(copy, client.user.avatarURL);
+    m.send({ embed: pbc })
+    msg.delete();
+    })
+    })
+    reaction2.on("collect", r => {
+    message.channel.send(`**Broadcast Canceled.**`).then(m => m.delete(5000));
+    msg.delete();
+    })
+    })
+    }
+    });
+
+client.on('message', message => {//Broadcast
+if (message.content.split(' ')[0] == '+bc')
+ message.guild.members.forEach( member => {
+         if (!message.member.hasPermission("ADMINISTRATOR"))  return;
+		 message.react("✅")
+member.send( `${member}, ` + message.content.substr(3));
+                                                            message.delete(1500);
+															
+});
+});
+
+client.login("proccess.env.BOT_TOKEN");
